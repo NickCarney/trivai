96203cf6e019d23884be97cecc1d24bc15995697
Nick Carney <npcarney@uvm.edu>
2025-04-12 19:15:57 -0400

    format podcast text and send to TTS route

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { useEffect } from "react";
 
+interface Trivia {
+  id: number;
+  question: string;
+  answer: string;
+}
+
 const now = new Date();
 const day = now.getDay();
+const categories: string[] = [];
+const questions: string[] = [];
+const answers: string[] = [];
+const facts: string[] = [];
+const podcastText: string[] = [];
 
 async function getTrivia(level: string) {
   const prompt = `You are a trivia host.
@@ -35,8 +46,49 @@ For ID 21-23 you will provide 3 random trivia facts unrelated to the categories 
   const trivia = await response.json();
 
   //   console.log(trivia);
+  const triviaFormatted = JSON.parse(trivia.split("```")[1]);
+  //   console.log(
+  //     "CATS:",
+  //     categories,
+  //     "QS:",
+  //     questions,
+  //     "ANSWERS:",
+  //     answers,
+  //     "FACTS:",
+  //     facts
+  //   );
+  return triviaFormatted;
+}
 
-  return trivia;
+async function generatePodcastText() {
+  const trivia = await getTrivia("hard");
+  for (let i = 0; i < trivia.length; i++) {
+    if (i < 20) {
+      if (i % 5 == 0) {
+        categories.push(trivia[i].category);
+        podcastText.push("Category " + trivia[i].category + " ... ... ...");
+      }
+      questions.push(trivia[i].question);
+      answers.push(trivia[i].answer);
+      podcastText.push(
+        `Question ${i + 1}: ` + trivia[i].question + " ... ... ..."
+      );
+      podcastText.push(`Answer ${i + 1}: ` + trivia[i].answer + " ... ... ...");
+    } else {
+      facts.push(trivia[i].fact);
+      podcastText.push(`Fact: ` + trivia[i].fact + " ... ... ...");
+    }
+  }
+  console.log("PODCAST TEXT" + podcastText);
+  const response = await fetch(`/api/tts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prompt: podcastText }),
+  });
+  const tts = await response.json();
+  console.log("tts success");
 }
 
 // useEffect(() => {}, [day]);
@@ -44,7 +96,7 @@ For ID 21-23 you will provide 3 random trivia facts unrelated to the categories 
 export default function Homepage() {
   return (
     <div className="">
-      <button onClick={() => getTrivia("Hard")}>get Trivai</button>
+      <button onClick={generatePodcastText}>get Trivai</button>
     </div>
   );
 }
